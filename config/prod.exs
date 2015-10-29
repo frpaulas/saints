ab884b101552d94bb6aa112203663dc2478e9bd0
Paul Sutcliffe <psutcliffe@ihispano.com>
2015-10-29 14:19:33 -0400

    added missing comma

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :saints, Saints.Endpoint,
         host: "nwsaints.herokuapp.com", 
         port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
