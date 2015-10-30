977b7a2c7c26dd5b02712509864f264b35a2d670
Paul Sutcliffe <psutcliffe@ihispano.com>
2015-10-30 08:15:08 -0400

    try removing port: arg from prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,8 @@ use Mix.Config
 config :saints, Saints.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", 
-        host: "nwsaints.herokuapp.com", 
-        port: 443],
+        host: "nwsaints.herokuapp.com"]
+#        port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
